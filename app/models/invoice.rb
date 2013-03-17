9cfd008209f38e6fab84db4d95fdb37a39544f78
Andy Mathys <andy@mindloop.be>
2013-03-17 16:01:55 +0100

    Refactored some logic out of the view

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,16 @@
 class Invoice < ActiveRecord::Base
   belongs_to :partner
   attr_accessible :creation_date, :description, :number, :partner_id, :payment_date, :total, :vat
+
+  def number_of_days
+      payment_date.mjd - creation_date.mjd
+  end
+
+  def vat_amount
+     total * Float(vat) / 100 
+  end
+
+  def total_amount
+    '%.2f' % (total * (1.00 +  Float(vat) / 100))
+  end
 end
